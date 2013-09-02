f951d73fdde3f6853e35f6106a2cd902c78a3d8e
Yoctotool <victor.iwaniack@gmail.com>
2013-09-02 18:21:11 +0200

    Anti-doublon pour les IDs
    On ne peut pas créer de message avec le même ID qu'un autre ou nommer
    l'ID time, date, type ou msg

--- simpbroad/src/io/github/yoctotool/simpbroad/Simpbroad.java
+++ simpbroad/src/io/github/yoctotool/simpbroad/Simpbroad.java
@@ -1,5 +1,9 @@
 package io.github.yoctotool.simpbroad;
 
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -26,11 +30,12 @@ public class Simpbroad extends JavaPlugin {
 				if(args[0].equalsIgnoreCase("new")) {
 					
 					//on vérifie, pour éviter les bugs, que l'id ne corresponde pas à l'un des noeuds du config.yml existant
-					if(args[2].equalsIgnoreCase("msg") || args[2].equalsIgnoreCase("type") || args[2].equalsIgnoreCase("time") || args[2].equalsIgnoreCase("hour")) {
-						sender.sendMessage("Message's ID can't be msg, type, time or hour.");
+					if(args[2].equalsIgnoreCase("msg") || args[2].equalsIgnoreCase("type") || args[2].equalsIgnoreCase("time") || doesIdExist(args[2])) {
+						sender.sendMessage("Message's ID can't be msg, type, time, date or an already existing one.");
 						return false;
 					}
 					
+					//traitement de la commande et enregistrement des messages
 					else if(args[1].equalsIgnoreCase("normal")) {
 						this.getConfig().set("messages."+args[2]+".type", "normal");
 						this.getConfig().set("messages."+args[2]+".msg", concatMsg(args));
@@ -47,8 +52,10 @@ public class Simpbroad extends JavaPlugin {
 						sender.sendMessage("The message is saved");
 						return true;
 					}
+					
+					//Une erreur? :O
 					else {
-						sender.sendMessage("ERREUR");
+						sender.sendMessage("ERROR: help with /sb");
 						return false;
 					}
 				}
@@ -64,5 +71,18 @@ public class Simpbroad extends JavaPlugin {
 			msg += args[i] + " ";
 		}
 		return msg;
+	}
+	//Fonction qui regarde tous les IDs entrés puis les comparent au nouvel ID du message 
+	public boolean doesIdExist(String arg) {
+		Map<String, Object> existingID = this.getConfig().getConfigurationSection("messages").getValues(false);
+		Iterator<Entry<String, Object>> it = existingID.entrySet().iterator();
+		while(it.hasNext()) {
+			Map.Entry ids = (Map.Entry)it.next();
+			if(((String)ids.getKey()).equalsIgnoreCase(arg)) {
+				return true;
+			}
+			it.remove();
+		}
+		return false;
 	}
 }
